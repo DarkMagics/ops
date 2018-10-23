ddbebd8a83543552cf1b786c38e061f16a09fca5
Denov <denov@Denovs-MacBook-Pro.local>
2018-10-23 13:52:28 +0300

    add tests

--- test/integration/default/controls/check_repo_spec.rb
+++ test/integration/default/controls/check_repo_spec.rb
@@ -1,8 +1,15 @@
-control 'check_website' do
-
-  describe http('https://github.com/DarkMagics/example-kitchen') do
-    its('status') { should cmp 200 }
-    its('body') { should match 'My awesome ghrepo' }
+control 'check_repo' do
+  
+  describe host('github.com', port: 443, protocol: 'tcp') do
+    it { should be_reachable }
+    it { should be_resolvable }
+    its('ipaddress') { should include '192.30.253.112' }
+    its('connection') { should_not match /connection refused/ }
   end
 
+  describe http('https://github.com/DarkMagics/example') do
+    its('status') { should cmp 200 }
+    its('body') { should match 'My awesome ghrepo' }
+    its('headers.Content-Type') { should match 'text/html' }
+  end
 end
